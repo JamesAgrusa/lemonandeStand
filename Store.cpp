cfef1f80e7c8be958ab57966a58b04ff64753310
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-03-18 06:35:15 -0400

    Add files via upload

--- Store.cpp
+++ Store.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
+#include <string>
 #include "UserInterface.h"
 #include "Game.h"
 #include "Store.h"
 #include "Inventory.h"
 
+using namespace std;
+
 Store::Store()
 {
 
+}
+
+double Store::sellLemon()
+{
+	
+	
+	return 0;
+}
+
+int Store::numberItemsToPurchase()
+{
+	int numberOfItems;
+	cout << "how many items would you like?" << endl;
+	cin >> numberOfItems;
+
+	return numberOfItems;
+}
+
+char Store::stillBuying()
+{
+	Game game;
+	char choice;
+	cout << "Are you done buying product? Yes(Y) or No(N)" << endl;
+	cin >> choice;
+	if (choice == 'Y')
+	{
+		game.goToStore();
+	}	
+	return choice;
+}
+
+void Store::itemAquired()
+{
+	for (int i = 0; i < numberOfItems; i++)
+	{
+
+	}
 }
--- Store.h
+++ Store.h
@@ -1,7 +1,16 @@
 class Store
 {
 public:
+	int numberOfItems{};
+	
+	double saleCost;
 
 	Store();
+
+
+	void itemAquired();
+	char stillBuying();
+	int numberItemsToPurchase();
+	double sellLemon();
 
 };
